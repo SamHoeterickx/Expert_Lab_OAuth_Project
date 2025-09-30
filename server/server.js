a4764d557f2e9b1ffcf1bb64fa31c9915f470256
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-09-30 16:05:15 +0200

    Added save logged in users in database

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const port = 3000;
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri)
 const DB_NAME = "Google_OAuth_Test";
+const database = client.db(DB_NAME);
 
 //Setup session
 //Resave false --> doesn't resave when session isnt changed
@@ -28,25 +29,53 @@ app.use(passport.session());
 
 app.use(express.json());
 
-const main = async() => {
-    await client.connect();
-    console.log('Connected successfully to server');
 
-    const db = client.db(DB_NAME);
-    console.log('Connected successfully to database');
+const startServer = async () => {
+    try {
+        await client.connect();
+        console.log('Connected successfully to MongoDB server');
+        app.listen(port, () => {
+            console.log(`Example of app is listening on port: ${port}`);
+        });
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1);
+    }
+};
 
-    return 'done.'
-
-}
-
-main().then(console.log).catch(console.error).finally(() => client.close());
+startServer();
 
 passport.use( new googleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/callback"
-}, (accesToken, refreshToken, profile, done) => {
-    return done(null, profile)
+}, async (accesToken, refreshToken, profile, done) => {
+
+    try{
+        const collection = database.collection('users');
+
+        let user = await collection.findOne({googleId: profile.id});
+
+        if(!user){
+            const newUser = {
+                googleId: profile.id,
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                photo: profile.photos[0].value
+            }
+
+            const result = await collection.insertOne(newUser);
+
+            user = { _id: result.insertedId, ...newUser };
+
+            return done(null, user);
+
+        } else return done(null, user);
+
+    }
+    catch(error){
+        console.error(error);
+    }
 }))
 
 passport.serializeUser(( user, done) => {
@@ -77,7 +106,7 @@ app.get('/profile', (req, res) => {
     console.log(req.user);
     try{
         if(req.user){
-            res.send(`<h1>Welcome ${req.user.displayName}</h1><a href="/logout">logout</a>`)
+            res.send(`<h1>Welcome ${req.user.name}</h1><a href="/logout">logout</a>`)
         }else {
             res.status(404).redirect('/');
         }
@@ -92,8 +121,4 @@ app.get("/logout", (req, res, next) => {
         if (err) { return next(err); }
         res.redirect("/");
     });
-})
-
-app.listen( port, () => {
-    console.log(`Example of app is listening on port: ${port}`);
 })
