356d45bf1df084a28ef050c8c91edfe9fa6eb266
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-09-30 15:34:45 +0200

    WIP Google OAuth, added init for passport

--- server/server.js
+++ server/server.js
@@ -1,13 +1,48 @@
+require('dotenv').config();
+
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const googleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 const port = 3000;
 
+//Setup session
+//Resave false --> doesn't resave when session isnt changed
+//saveUninitialized --> save new input session
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello world");
+
+passport.use( new googleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+}, (accesToken, refreshToken, profile, done) => {
+    return done(null, profile)
+}))
+
+passport.serializeUser(( user, done) => {
+    return done(null, user);
 })
+
+passport.deserializeUser((user, done) => {
+    return done(null, user);
+})
+
+app.get('/', (req, res) => {
+    res.send("<a href='/auth/google'>Login with Google</a>");
+})
+
 
 app.listen( port, () => {
     console.log(`Example of app is listening on port: ${port}`);
