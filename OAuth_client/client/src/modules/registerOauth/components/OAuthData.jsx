d6c43e75d92b2b2254353c24518a7305b835f01f
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-10-20 11:45:11 +0200

    Chore(registerOauth): Added overview of data

---
+++ OAuth_client/client/src/modules/registerOauth/components/OAuthData.jsx
@@ -0,0 +1,47 @@
+import { clsx } from "clsx";
+import styles from "../page/registerOauth.module.scss";
+
+export const OAuthData = ({ client_id, client_secret, redirect_uri, base_uri, scope }) => {
+  return (
+    <section className={clsx(styles["oauth-data-wrapper"])}>
+        <h3>OAuth Client Details</h3>
+
+        <p>
+            <strong>Client ID</strong>
+            {client_id || "—"}
+        </p>
+
+        <p>
+            <strong>Client Secret</strong>
+            {client_secret || "—"}
+        </p>
+
+        <p>
+            <strong>Redirect URI</strong>
+            {redirect_uri || "—"}
+        </p>
+
+        <p>
+            <strong>Base URI</strong>
+            {base_uri || "—"}
+        </p>
+
+        <div>
+            <strong>Scope</strong>
+            <div className={clsx(styles["scope-list"])}>
+                {
+                    scope && scope.length > 0 ? (
+                        scope.map((s) => (
+                        <span key={s} className={clsx(styles["scope-item"])}>
+                            {s}
+                        </span>
+                        ))
+                    ) : (
+                        <span className={clsx(styles["scope-item"])}>None</span>
+                    )
+                }
+            </div>
+        </div>
+    </section>
+  );
+};
